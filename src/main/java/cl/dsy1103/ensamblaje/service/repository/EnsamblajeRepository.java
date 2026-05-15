5fab6a43d31bf53941495af57b5574f0a33a8ff2
Matias Tiznado <tiznado@duocuc.cl>
2026-05-14 23:55:56 -0400

    Embalage V3 arreglos de los service

--- src/main/java/cl/dsy1103/ensamblaje/service/repository/EnsamblajeRepository.java
+++ src/main/java/cl/dsy1103/ensamblaje/service/repository/EnsamblajeRepository.java
@@ -2,11 +2,10 @@ package cl.dsy1103.ensamblaje.service.repository;
 
 import cl.dsy1103.ensamblaje.service.model.Ensamblaje;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import java.util.List;
 
-@Repository
 public interface EnsamblajeRepository extends JpaRepository<Ensamblaje, Long> {
 
     // Buscar todos los ensamblajes (tickets) de un tecnico específico
--- src/main/java/cl/dsy1103/ensamblaje/service/repository/FaseEnsamblajeRepository.java
+++ src/main/java/cl/dsy1103/ensamblaje/service/repository/FaseEnsamblajeRepository.java
@@ -2,11 +2,11 @@ package cl.dsy1103.ensamblaje.service.repository;
 
 import cl.dsy1103.ensamblaje.service.model.FaseEnsamblaje;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import java.util.List;
 
-@Repository
+
 public interface FaseEnsamblajeRepository extends JpaRepository<FaseEnsamblaje, Long> {
 
     // Buscar todas las fases que pertenecen a un mismo ticket de ensamblaje.
